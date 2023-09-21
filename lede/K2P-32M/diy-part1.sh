17b394cb902c4dc40e06f92f23f96c9bd81ffe4c
lmq8267 <119713693+lmq8267@users.noreply.github.com>
2023-09-21 22:40:47 +0800

    Update diy-part1.sh

--- lede/K2P-32M/diy-part1.sh
+++ lede/K2P-32M/diy-part1.sh
@@ -9,11 +9,12 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
+cd /opt/openwrt
 ##### K2P-32M修改编译文件 ######
-sed -i 's/"Phicomm K2P";/"Phicomm K2P-32M";/g' ./target/linux/ramips/dts/mt7621_phicomm_k2p.dts
-sed -i '/spi-max-frequency/a\\t\tbroken-flash-reset;' ./target/linux/ramips/dts/mt7621_phicomm_k2p.dts
-sed -i 's/<0xa0000 0xf60000>/<0xa0000 0x1fb0000>/g' ./target/linux/ramips/dts/mt7621_phicomm_k2p.dts
-sed -i 's/15744k/32448k/g' ./target/linux/ramips/image/mt7621.mk
+sed -i 's/"Phicomm K2P";/"Phicomm K2P-32M";/g' /opt/openwrt/target/linux/ramips/dts/mt7621_phicomm_k2p.dts
+sed -i '/spi-max-frequency/a\\t\tbroken-flash-reset;' /opt/openwrt/target/linux/ramips/dts/mt7621_phicomm_k2p.dts
+sed -i 's/<0xa0000 0xf60000>/<0xa0000 0x1fb0000>/g' /opt/openwrt/target/linux/ramips/dts/mt7621_phicomm_k2p.dts
+sed -i 's/15744k/32448k/g' /opt/openwrt/target/linux/ramips/image/mt7621.mk
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
