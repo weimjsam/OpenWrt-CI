32e47926bdd1c0ef7eeb1c2cc45f2b931a19d556
lmq8267 <119713693+lmq8267@users.noreply.github.com>
2023-09-21 22:41:13 +0800

    Update diy-part2.sh

--- lede/K2P-32M/diy-part2.sh
+++ lede/K2P-32M/diy-part2.sh
@@ -9,7 +9,7 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-
+cd /opt/openwrt
 ##### Modify default IP
 #sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 ##### Modify hostname
