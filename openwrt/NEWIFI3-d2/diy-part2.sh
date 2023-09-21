2fcd1152e4bd379d530ac1d337b90cfbb2354f72
lmq8267 <119713693+lmq8267@users.noreply.github.com>
2023-09-21 22:05:00 +0800

    Update diy-part2.sh

--- openwrt/NEWIFI3-d2/diy-part2.sh
+++ openwrt/NEWIFI3-d2/diy-part2.sh
@@ -10,8 +10,17 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+###### Modify default IP
+#sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+
+###### Modify hostname
+sed -i 's/OpenWrt/OpenWrt_NEWIFI3/g' package/base-files/files/bin/config_generate
+
+####### Modify the version number
+sed -i "s/OpenWrt /Steven build from Lede $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+
+####### Set argon as default theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 ##-----------------Add OpenClash dev core------------------
 #curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-mipsle-softfloat.tar.gz -o /tmp/clash.tar.gz
 #tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
