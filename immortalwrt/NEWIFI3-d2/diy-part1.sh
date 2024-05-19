7444af348eb045406eb0bbabac80923f8ee32d66
weimjsam <weimjsam@qq.com>
2024-05-19 19:39:39 +0800

    Update diy-part1.sh

--- immortalwrt/NEWIFI3-d2/diy-part1.sh
+++ immortalwrt/NEWIFI3-d2/diy-part1.sh
@@ -14,8 +14,8 @@ cd /opt/openwrt
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small-package' >>feeds.conf.default
 #echo 'src-git haibo https://github.com/haiibo/openwrt-packages' >>feeds.conf.default
 #echo 'src-git liuran001 https://github.com/liuran001/openwrt-packages' >>feeds.conf.default
